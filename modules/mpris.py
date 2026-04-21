99dcb7c45506147ea81169eaae67e8c0cd37e658
VladVirnyi <vladvwv15@gmail.com>
2026-04-21 22:39:13 +0300

    refactor: second attempt to integrate in DEs

--- modules/mpris.py
+++ modules/mpris.py
@@ -3,35 +3,47 @@
 
 from mpris_server.adapters import MprisAdapter
 from mpris_server.server import Server
+import threading
 
-class MprisAdapter(MprisAdapter):
+class KurtMprisAdapter(MprisAdapter):
     def __init__(self, player):
         self.player = player
         super().__init__()
 
     # mpris identity for .desktop file
-    def get_desktop_entry(self):
+    @property
+    def Identity(self):
+        return "Kurt Player"
+    @property
+    def DesktopEntry(self):
         return "kurt"
     
     # player status
-    def get_playback_status(self):
+    def playback_status(self):
         """returns 'Playing', 'Paused' or 'Stopped'"""
         if self.player.pause:
             return "Paused"
-        return "Playing"
+        else:
+            return "Playing"
 
-    def get_metadata(self):
+    def metadata(self):
         """returns a dictionary of metadata according to the MPRIS 2 specification"""
-        return {
-            'mpris:trackid': '/org/mpris/MediaPlayer2/Track/0',
-            'xesam:title': str(self.player.metadata.get('title', 'Unknown')),
-            'xesam:artist': [str(self.player.metadata.get('artist', 'Unknown'))],
-            'xesam:album': str(self.player.metadata.get('album', '')),
-            'xesam:genre': [str(self.player.metadata.get('genre', ''))],
+        if not self.player or not hasattr(self.player, 'metadata'):
+            return {'mpris:trackid': '/org/mpris/MediaPlayer2/Track/0'}
+        
+        try:
+            return {
+                'mpris:trackid': '/org/mpris/MediaPlayer2/Track/0',
+                'xesam:title': str(self.player.metadata.get('title', 'Unknown')),
+                'xesam:artist': [str(self.player.metadata.get('artist', 'Unknown'))],
+                'xesam:album': str(self.player.metadata.get('album', '')),
+                'xesam:genre': [str(self.player.metadata.get('genre', ''))],
 
-            # If you are fetching album art locally:
-            # 'mpris:artUrl': f"file:///path/to/album_art.jpg" 
-        }
+                # If you are fetching album art locally:
+                # 'mpris:artUrl': f"file:///path/to/album_art.jpg" 
+            }
+        except Exception as e:
+            return {'mpris:trackid': '/org/mpris/MediaPlayer2/Track/0'}
 
     # Control (these methods will be called by GNOME)
     def play(self):
@@ -46,20 +58,3 @@ class MprisAdapter(MprisAdapter):
     def previous(self):
         self.player.playlist_prev()
 
-
-# Function for setting up MPRIS
-def setup_mpris(player):
-    adapter = MprisAdapter(player)
-    # Register the player with the system (GNOME will see it as "kurt")
-    mpris_server = Server('kurt', adapter=adapter)
-    mpris_server.publish()
-
-    @player.property_observer('pause')
-    def on_pause_change(_name, value):
-        mpris_server.emit_changes('org.mpris.MediaPlayer2.Player', ['PlaybackStatus'])
-
-    @player.property_observer('metadata')
-    def on_metadata_change(_name, value):
-        mpris_server.emit_changes('org.mpris.MediaPlayer2.Player', ['Metadata'])
-
-    return mpris_server
