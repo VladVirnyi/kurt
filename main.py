4a0b96d26933621762d2ed8c5b3797cd3953ed98
VladVirnyi <vladvwv15@gmail.com>
2026-04-26 22:31:56 +0300

    Revert "refactor: second attempt to integrate in DEs"
    This reverts commit 99dcb7c45506147ea81169eaae67e8c0cd37e658.

--- main.py
+++ main.py
@@ -2,22 +2,31 @@
 import argparse
 import sys
 import asyncio
-import threading
 
-from modules import player
-from modules.mpris import KurtMprisAdapter, Server
-from modules.mpris_setup import setup_mpris
+from modules.mpris import MprisAdapter, setup_mpris
 from kurt_shell import KurtAsyncShell
-
 
 def main():
     # argparse settings
     parser = argparse.ArgumentParser(prog='kurt', description='Minimalist Async CLI Music Player')
+    # parser.add_argument('-sp', '--spotify', action='store_true', help='Use Spotify as search platform')
+    # parser.add_argument('-so', '--soundcloud', action='store_true', help='Use Soundcloud as search platform')
     parser.add_argument('-yo', '--youtube', action='store_true', help='Use Youtube as search platform (default)')
 
     args = parser.parse_args()
 
     shell = KurtAsyncShell()
+
+    #flags (i guess kurt wil use only youtube search)
+    # if args.spotify:
+    #     print("Spotify mode activated")
+    #     # in future we can use dependency injection
+    #     # shell.youtube_search = SpotifySearch()
+    # elif args.soundcloud:
+    #     print("Soundcloud mode activated")
+    #     # shell.youtube_search = SoundcloudSearch()
+    # else:
+    #     pass
 
     mpris = None  # Initialize mpris variable
     try:
@@ -36,8 +45,6 @@ def main():
         print("\nGoodbye!")
     finally:
         pass
-
-
 
 if __name__ == '__main__':
     main()
--- modules/mpris.py
+++ modules/mpris.py
@@ -3,19 +3,14 @@
 
 from mpris_server.adapters import MprisAdapter
 from mpris_server.server import Server
-import threading
 
-class KurtMprisAdapter(MprisAdapter):
+class MprisAdapter(MprisAdapter):
     def __init__(self, player):
         self.player = player
         super().__init__()
 
     # mpris identity for .desktop file
-    @property
-    def Identity(self):
-        return "Kurt Player"
-    @property
-    def DesktopEntry(self):
+    def get_desktop_entry(self):
         return "kurt"
 
     def can_quit(self):
@@ -66,31 +61,24 @@ class KurtMprisAdapter(MprisAdapter):
         setattr(self.player, "pause", paused)
     
     # player status
-    def playback_status(self):
+    def get_playback_status(self):
         """returns 'Playing', 'Paused' or 'Stopped'"""
         if self.player.pause:
             return "Paused"
-        else:
-            return "Playing"
+        return "Playing"
 
-    def metadata(self):
+    def get_metadata(self):
         """returns a dictionary of metadata according to the MPRIS 2 specification"""
-        if not self.player or not hasattr(self.player, 'metadata'):
-            return {'mpris:trackid': '/org/mpris/MediaPlayer2/Track/0'}
-        
-        try:
-            return {
-                'mpris:trackid': '/org/mpris/MediaPlayer2/Track/0',
-                'xesam:title': str(self.player.metadata.get('title', 'Unknown')),
-                'xesam:artist': [str(self.player.metadata.get('artist', 'Unknown'))],
-                'xesam:album': str(self.player.metadata.get('album', '')),
-                'xesam:genre': [str(self.player.metadata.get('genre', ''))],
+        return {
+            'mpris:trackid': '/org/mpris/MediaPlayer2/Track/0',
+            'xesam:title': str(self.player.metadata.get('title', 'Unknown')),
+            'xesam:artist': [str(self.player.metadata.get('artist', 'Unknown'))],
+            'xesam:album': str(self.player.metadata.get('album', '')),
+            'xesam:genre': [str(self.player.metadata.get('genre', ''))],
 
-                # If you are fetching album art locally:
-                # 'mpris:artUrl': f"file:///path/to/album_art.jpg" 
-            }
-        except Exception as e:
-            return {'mpris:trackid': '/org/mpris/MediaPlayer2/Track/0'}
+            # If you are fetching album art locally:
+            # 'mpris:artUrl': f"file:///path/to/album_art.jpg" 
+        }
 
     # Control (these methods will be called by GNOME)
     def play(self):
@@ -109,3 +97,20 @@ class KurtMprisAdapter(MprisAdapter):
         if self.player and hasattr(self.player, 'playlist_prev'):
             self.player.playlist_prev()
 
+
+# Function for setting up MPRIS
+def setup_mpris(player):
+    adapter = MprisAdapter(player)
+    # Register the player with the system (GNOME will see it as "kurt")
+    mpris_server = Server('kurt', adapter=adapter)
+    mpris_server.publish()
+
+    @player.property_observer('pause')
+    def on_pause_change(_name, value):
+        mpris_server.emit_changes('org.mpris.MediaPlayer2.Player', ['PlaybackStatus'])
+
+    @player.property_observer('metadata')
+    def on_metadata_change(_name, value):
+        mpris_server.emit_changes('org.mpris.MediaPlayer2.Player', ['Metadata'])
+
+    return mpris_server
